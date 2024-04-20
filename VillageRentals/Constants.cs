cd539fcbb91181eccfeddcf3c4e0ae37ce3566dc
Gabriel Windows <alvesgabriel1994@gmail.com>
2024-04-20 02:31:36 -0600

    fix: simplify database path for mobile application

--- VillageRentals/Constants.cs
+++ VillageRentals/Constants.cs
@@ -12,24 +12,5 @@ internal class Constants
         // enable multi-threaded database access
         SQLite.SQLiteOpenFlags.SharedCache;
 
-    public static string DatabasePath
-    {
-        get
-        {
-            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
-            DirectoryInfo directory = new(baseDirectory);
-
-            while (directory.Name != "VillageRentals")
-            {
-                directory = directory.Parent!;
-
-                if (directory is null) throw new InvalidOperationException("Could not find the project root directory.");
-            }
-
-            string root = directory.FullName;
-            string dataDirectory = Path.Combine(root, "Data");
-
-            return Path.Combine(dataDirectory, DatabaseFilename);
-        }
-    }
+    public static string DatabasePath => Path.Combine(AppDomain.CurrentDomain.BaseDirectory, DatabaseFilename);
 }
