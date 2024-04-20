42d5089d9e63dac15f5ce682263b277aeb0ce0e3
Gabriel Windows <alvesgabriel1994@gmail.com>
2024-04-19 23:26:17 -0600

    feat: define models

---
+++ VillageRentals/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿namespace VillageRentals.Models;
+
+internal enum Category
+{
+    PowerTools = 10,
+
+    YardEquipment = 20,
+
+    Compressors = 30,
+
+    Generators = 40,
+
+    AirTools = 50,
+}
---
+++ VillageRentals/Models/Equipment.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+
+namespace VillageRentals.Models;
+
+internal class Equipment
+{
+    [PrimaryKey]
+    public int Id { get; set; }
+
+    public Category Category { get; set; }
+
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+}
